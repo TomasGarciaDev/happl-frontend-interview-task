006c284fb9b34ad7550e9bd933169b87de9a30f6
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-01-16 18:35:26 +0100

    Style: Card headline line height

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,7 +3,9 @@ import { Allowance } from '@/types/allowances'
 const Card = ({ allowance }: { allowance: Allowance }) => {
   return (
     <div className="bg-white rounded-lg border border-gray-200 p-4">
-      <h3 className="font-medium text-base text-nowrap">{allowance.name}</h3>
+      <h3 className="font-medium text-base leading-6 text-nowrap">
+        {allowance.name}
+      </h3>
     </div>
   )
 }
