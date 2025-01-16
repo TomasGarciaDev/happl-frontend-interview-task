5c506b43be6c5dbbecbe7f6898d99fe657d98b2d
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-01-16 15:40:33 +0100

    Style: Inter font setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
 import '@/styles/main.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactElement) => React.ReactNode
@@ -13,7 +16,11 @@ type AppPropsWithLayout = AppProps & {
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default App
